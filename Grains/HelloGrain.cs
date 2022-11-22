720b01c9c018ed734061fa60492a833b47ffa615
Justin Jones <justin.jones@lumiglobal.com>
2022-11-22 13:46:19 +0000

    very basic setup with table storage

---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,49 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans.Runtime;
+using System.Reflection;
+
+namespace Grains
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger _logger;
+        private readonly IPersistentState<HelloGrainState> _state;
+
+        public HelloGrain(ILogger<HelloGrain> logger,
+            [PersistentState("hellostate", "SimpleTableStorage1")] IPersistentState<HelloGrainState> state
+            )
+        {
+            _logger = logger;
+            _state = state;
+        }
+
+        public override Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            var grainId = this.GetGrainId();
+
+            if (String.IsNullOrWhiteSpace(this._state?.State?.LastMessage))
+            {
+                _logger.LogInformation($"LOADING {grainId} - New");
+            }
+            else
+            {
+                _logger.LogInformation($"LOADING {grainId} - msg={this._state?.State?.LastMessage}");
+            }
+            return base.OnActivateAsync(cancellationToken);
+        }
+
+        async Task<string> IHello.SayHello(string greeting)
+        {
+            this._state.State.LastMessage = greeting;
+            await this._state.WriteStateAsync();
+            _logger.LogInformation("SayHello message received: greeting = '{Greeting}'", greeting);
+            return $"\n Client said: '{greeting}', so HelloGrain says: Hello!";
+        }
+    }
+
+    public class HelloGrainState
+    {
+        public string LastMessage { get; set; }
+    }
+}
---
+++ Silo/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+
+using Orleans.Runtime;
+
+try
+{
+    var host = await StartSiloAsync();
+    Console.WriteLine("\n\n Press Enter to terminate...\n\n");
+    Console.ReadLine();
+
+    await host.StopAsync();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    return 1;
+}
+
+static async Task<IHost> StartSiloAsync()
+{
+
+    var builder = new HostBuilder()
+        .UseOrleans(c =>
+        {
+            c.UseLocalhostClustering()
+            .AddAzureTableGrainStorage(name: "SimpleTableStorage1", o =>
+            {
+                o.ConfigureTableServiceClient("UseDevelopmentStorage=true");
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = "dev";
+                options.ServiceId = "OrleansBasics";
+            })
+            .ConfigureLogging(logging => logging.AddConsole());
+        });
+
+    var host = builder.Build();
+    await host.StartAsync();
+
+    return host;
+}
